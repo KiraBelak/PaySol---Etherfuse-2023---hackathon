323179c3cbfb49151a177fc3c40992d630a814e5
KiraBelak <kalebchivas@hotmail.com>
2023-02-25 23:16:19 -0600

    boton signin mobil

--- components/common/Header.js
+++ components/common/Header.js
@@ -185,13 +185,18 @@ const Header = () => {
         leaveFrom="opacity-100 scale-100"
         leaveTo="opacity-0 scale-95"
       >
+        
         <Popover.Panel
           focus
           className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
         >
+          
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            
             <div className="pt-5 pb-6 px-5 sm:pb-8">
+              
               <div className="flex items-center justify-between">
+                
                 <div>
                   <Link href="/">
                     <a>
@@ -199,8 +204,120 @@ const Header = () => {
                       <img className="h-8 w-auto" src={logoUrl} alt="logo" />
                     </a>
                   </Link>
+                  
                 </div>
+                <div className="flex items-center md:ml-12">  
+         {user ? (
+              <Menu as="div" className="ml-3 relative">
+              <div>
+                <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-offset-2  focus:ring-white">
+                  <span className="sr-only">Open user menu</span>
+                  {user.image ? (
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={session.user.image}
+                      alt=""
+                    />
+                  ) : (
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={`https://avatars.dicebear.com/api/micah/${user.email}.svg?background=%23ffffff`}
+                      alt=""
+                    />
+                  )}
+                </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-40">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link href="/user/profile">
+                            <a
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Mi Cuenta
+                            </a>
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link href="/user/pagar">
+                            <a
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Pagar
+                            </a>
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link href="/user/pagos">
+                            <a
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Pagos
+                            </a>
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link href="/user/grupos">
+                            <a
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Grupos
+                            </a>
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <div
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                            )}
+                            onClick={handleLogout}
+                          >
+                            Cerrar sesion
+                          </div>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              ) 
+         : (  
+                <Link href="/">
+                  <a onClick={login}>Sign In </a>
+                </Link>
+        )}
+            </div>
                 <div className="-mr-2">
+                  
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <XIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
@@ -208,6 +325,7 @@ const Header = () => {
               </div>
             </div>
             <div className="py-6 px-5">
+           
               <div className="grid grid-cols-2 gap-4">
                 {navigation.pages.map((page) => (
                   <Link key={page.name} href={page.href}>
